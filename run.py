64c92ff3d74f8616f60d9461a70f924d144b7f69
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-09 01:51:07 +0300

    git add method to collect user data

--- run.py
+++ run.py
@@ -3,3 +3,9 @@ from credentials import Credentials
 import random
 import string
 
+user_data =None
+Credentials_data = None
+def signup(user_name, password, email, p_number):
+    user_data = User(user_name, password, email, p_number)
+    return user_data
+
