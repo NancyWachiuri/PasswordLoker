329c9c06db92af05986ec69eeda3b1b714bf9602
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-09 05:35:29 +0300

    correction of definition

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, username,password,email,phone_number):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.phone_number = phone_number
+
+#def add_user(self):
+        #User.user.append(self)
+        
