0d0f1c5dfac1da8143a75a015706c83f40c01622
NancyWachiuri <nancywachiuri@gmail.com>
2021-09-16 10:45:26 +0300

    Cache files

--- __pycache__/passwordlocker_test.py
+++ __pycache__/passwordlocker_test.py
@@ -1,13 +1,1 @@
-import unittest # Importing the unittest module
-from user import User # Importing the contact class
 
-
-class TestUser(unittest.TestCase):
-
-
-    '''
-    Test class that defines test cases for the contact class behaviours.
-
-    Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-    '''
